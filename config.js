38196aef5354d17da48bfde415be7e4eecf1a2ce
sushanth <sushanth.nukala14@gmail.com>
2021-03-19 12:10:29 +0530

    OAuth using passport-facebook-token

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '245758153942402',
+        clientSecret : '67c34faf79dbab885bd0a8649c1d693c'
+    }
 }
